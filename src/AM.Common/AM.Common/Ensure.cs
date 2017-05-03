4e1a69c2b9ed1dbf2340c6789a257958d13b22e0
Artak Mkrtchyan <artak@outlook.com>
2017-05-03 09:26:42 -0700

    -Added the Ensure context type - Added ValidationExtensions.

---
+++ src/AM.Common/AM.Common/Ensure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AM.Common.ValidationFramework
+{
+    public sealed class Ensure<T>
+    {
+        private T parameterValue;
+
+        internal T ParameterValue { get => this.parameterValue; }
+
+        internal Ensure(T parameterValue)
+        {
+            this.parameterValue = parameterValue;
+        }
+    }
+}
---
+++ src/AM.Common/AM.Common/ValidationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AM.Common.ValidationFramework
+{
+    public static class ValidationExtensions
+    {
+        public static Ensure<T> Ensure<T>(this T parameter)
+        {
+            return new Ensure<T>(parameter);
+        }
+
+        public static Ensure<T> IsNotNull<T>(this Ensure<T> context) where T : class
+        {
+            if (context.ParameterValue == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return context;
+        }
+
+        public static Ensure<string> IsNotNullOrEmpty(this Ensure<string> context)
+        {
+            if (String.IsNullOrEmpty(context.ParameterValue))
+            {
+                throw new ArgumentException();
+            }
+
+            return context;
+        }
+    }
+}
