74161a1fe03d92f93050c3521ac98561a948e14f
Artak Mkrtchyan <artak@outlook.com>
2017-05-05 10:18:22 -0700

    Added some comments to the types and their members.

--- src/AM.Common/AM.Common/Ensure.cs
+++ src/AM.Common/AM.Common/Ensure.cs
@@ -2,6 +2,10 @@
 
 namespace AM.Common.ValidationFramework
 {
+    /// <summary>
+    /// Represents the validaiton context.
+    /// </summary>
+    /// <typeparam name="T">The type parameter of the argument tp be validated.</typeparam>
     public sealed class Ensure<T>
     {
         private T parameterValue;
--- src/AM.Common/AM.Common/EnumerableValidationExtensions.cs
+++ src/AM.Common/AM.Common/EnumerableValidationExtensions.cs
@@ -6,6 +6,12 @@ namespace AM.Common.ValidationFramework
 {
     public static class EnumerableValidationExtensions
     {
+        /// <summary>
+        /// Validates the specified <see cref="IEnumerable{T}" /> making sure it's not null or empty. 
+        /// </summary>
+        /// <typeparam name="T">The type parameter for the enumerable items.</typeparam>
+        /// <param name="context">The context the validation will be happening in.</param>
+        /// <returns></returns>
         public static Ensure<IEnumerable<T>> IsNotEmptyEnumerable<T>(this Ensure<IEnumerable<T>> context)
         {
             context.IsNotNull();
